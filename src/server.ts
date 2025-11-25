fadd4101624b76a911c7a74f1b5916f5c6c260d6
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-11-26 00:49:08 +0600

    explore env configaration

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import http, {IncomingMessage, Server, ServerResponse} from 'http';
+import config from './config/index';
 const server: Server = http.createServer((req:IncomingMessage, res: ServerResponse)=>{
     console.log('server is running....')
 
@@ -12,6 +13,6 @@ const server: Server = http.createServer((req:IncomingMessage, res: ServerRespon
 
 })
 
-server.listen(5000, ()=>{
-    console.log(`server is running on port ${5000}`)
+server.listen(config.port, ()=>{
+    console.log(`server is running on port ${config.port}`)
 })
